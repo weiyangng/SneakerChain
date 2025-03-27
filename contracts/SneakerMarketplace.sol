f8e777f68ddae862c464626b53ab947cee7b8da6
javieryeow <javieryeow@gmail.com>
2025-03-27 11:37:12 +0800

    using ERC1155 for SneakerToken

--- contracts/SneakerMarketplace.sol
+++ contracts/SneakerMarketplace.sol
@@ -1,61 +1,60 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+import "./SneakerToken.sol";
 
 contract SneakerMarketplace {
-    struct Sneaker {
-        uint256 id;
-        string name;
-        uint256 price;
-        address owner;
+    SneakerToken sneakerTokenContract;
+    uint256 public commissionFee;
+    address _owner;
+    mapping(uint256 => uint256) listPrice;
+
+    event SneakerAdded(uint256 tokenId, address seller, uint256 price);
+    event SneakerPurchased(
+        uint256 tokenId,
+        address buyer,
+        address seller,
+        uint256 price
+    );
+
+    constructor(SneakerToken sneakerTokenAddress, uint256 fee) {
+        sneakerTokenContract = sneakerTokenAddress;
+        commissionFee = fee;
+        _owner = msg.sender;
     }
 
-    mapping(uint256 => Sneaker) private sneakers;
-    uint256 private nextSneakerId;
+    function listSneaker(uint256 tokenId, uint256 price) public {}
 
-    event SneakerAdded(uint256 indexed sneakerId, string name, uint256 price, address indexed owner);
-    event SneakerPurchased(uint256 indexed sneakerId, address indexed buyer, address indexed seller, uint256 price);
+    function purchaseSneaker(uint256 sneakerId) public payable virtual {}
 
-    function addSneaker(string memory name, uint256 price) public virtual {
-        sneakers[nextSneakerId] = Sneaker(nextSneakerId, name, price, msg.sender);
-        emit SneakerAdded(nextSneakerId, name, price, msg.sender);
-        nextSneakerId++;
-    }
+    function getSneaker(
+        uint256 sneakerId
+    ) public view returns (string memory name, uint256 price, address owner) {}
 
-    function purchaseSneaker(uint256 sneakerId) public payable virtual {
-        Sneaker storage sneaker = sneakers[sneakerId];
-        require(sneaker.owner != address(0), "Sneaker does not exist");
-        require(msg.value == sneaker.price, "Incorrect price");
-        require(sneaker.owner != msg.sender, "Cannot purchase your own sneaker");
+    // function getAllSneakers()
+    //     public
+    //     view
+    //     virtual
+    //     returns (
+    //         uint256[] memory ids,
+    //         string[] memory names,
+    //         uint256[] memory prices,
+    //         address[] memory owners
+    //     )
+    // {
+    //     uint256 length = nextSneakerId;
+    //     ids = new uint256[](length);
+    //     names = new string[](length);
+    //     prices = new uint256[](length);
+    //     owners = new address[](length);
 
-        address seller = sneaker.owner;
-        sneaker.owner = msg.sender;
+    //     for (uint256 i = 0; i < length; i++) {
+    //         Sneaker storage sneaker = sneakers[i];
+    //         ids[i] = sneaker.id;
+    //         names[i] = sneaker.name;
+    //         prices[i] = sneaker.price;
+    //         owners[i] = sneaker.owner;
+    //     }
 
-        payable(seller).transfer(msg.value);
-
-        emit SneakerPurchased(sneakerId, msg.sender, seller, sneaker.price);
-    }
-
-    function getSneaker(uint256 sneakerId) public view returns (string memory name, uint256 price, address owner) {
-        Sneaker storage sneaker = sneakers[sneakerId];
-        require(sneaker.owner != address(0), "Sneaker does not exist");
-        return (sneaker.name, sneaker.price, sneaker.owner);
-    }
-
-    function getAllSneakers() public view virtual returns (uint256[] memory ids, string[] memory names, uint256[] memory prices, address[] memory owners) {
-        uint256 length = nextSneakerId;
-        ids = new uint256[](length);
-        names = new string[](length);
-        prices = new uint256[](length);
-        owners = new address[](length);
-
-        for (uint256 i = 0; i < length; i++) {
-            Sneaker storage sneaker = sneakers[i];
-            ids[i] = sneaker.id;
-            names[i] = sneaker.name;
-            prices[i] = sneaker.price;
-            owners[i] = sneaker.owner;
-        }
-
-        return (ids, names, prices, owners);
-    }
+    //     return (ids, names, prices, owners);
+    // }
 }
---
+++ contracts/SneakerToken.sol
@@ -0,0 +1,112 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155URIStorage.sol";
+
+contract SneakerToken is ERC1155URIStorage {
+    uint256 public _tokenIds = 0;
+    address public contractOwner;
+    mapping(uint256 => address[]) public sneakerOwners;
+    mapping(uint256 => mapping(address => bool)) private isSneakerOwner;
+    mapping(uint256 => uint256) public maxShares;
+
+    event MintSNKT(
+        address to,
+        uint256 tokenId,
+        uint256 amount,
+        string metadataURI
+    );
+    event TransferSNKT(
+        address from,
+        address to,
+        uint256 tokenId,
+        uint256 amount
+    );
+    event BurnSNKT(address owner, uint256 tokenId, uint256 amount);
+
+    constructor() ERC1155("") {
+        contractOwner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(
+            msg.sender == contractOwner,
+            "Caller is not the owner of the contract"
+        );
+        _;
+    }
+
+    modifier sneakerOwner(uint256 tokenId) {
+        require(
+            balanceOf(msg.sender, tokenId) > 0,
+            "Caller does not own this sneaker"
+        );
+        _;
+    }
+
+    modifier validToken(uint256 tokenId) {
+        require(
+            tokenId != 0 && tokenId <= _tokenIds,
+            "This is not a valid token"
+        );
+        _;
+    }
+
+    function mintSneakerToken(
+        address to,
+        uint256 amount,
+        string memory metadataURI
+    ) public onlyOwner returns (uint256) {
+        uint256 newTokenId = _tokenIds++;
+        _mint(to, newTokenId, amount, "");
+        _setURI(newTokenId, metadataURI);
+        maxShares[newTokenId] = amount;
+        sneakerOwners[newTokenId].push(to);
+        emit MintSNKT(to, newTokenId, amount, metadataURI);
+        return newTokenId;
+    }
+
+    function transferSneakerToken(
+        address to,
+        uint256 tokenId,
+        uint256 amount
+    ) public sneakerOwner(tokenId) validToken(tokenId) {
+        require(balanceOf(msg.sender, tokenId) >= amount);
+        safeTransferFrom(msg.sender, to, tokenId, amount, "");
+        if (!isSneakerOwner[tokenId][to]) {
+            sneakerOwners[tokenId].push(to);
+            isSneakerOwner[tokenId][to] = true;
+        }
+        emit TransferSNKT(msg.sender, to, tokenId, amount);
+    }
+
+    function transferToMarket(
+        address to,
+        uint256 tokenId,
+        uint256 amount
+    ) public sneakerOwner(tokenId) validToken(tokenId) {
+        require(balanceOf(msg.sender, tokenId) >= amount);
+        safeTransferFrom(msg.sender, to, tokenId, amount, "");
+    }
+
+    function burnSneakerToken(
+        uint256 tokenId,
+        uint256 amount
+    ) public sneakerOwner(tokenId) validToken(tokenId) {
+        require(balanceOf(msg.sender, tokenId) >= amount);
+        _burn(msg.sender, tokenId, amount);
+        emit BurnSNKT(msg.sender, tokenId, amount);
+    }
+
+    function isOwnerOfSneaker(
+        uint256 tokenId
+    ) public view validToken(tokenId) returns (bool) {
+        return isSneakerOwner[tokenId][msg.sender];
+    }
+
+    function getSneakerMetadata(
+        uint256 tokenId
+    ) public view validToken(tokenId) returns (string memory) {
+        return uri(tokenId);
+    }
+}
